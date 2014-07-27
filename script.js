aa86de915f579fc59af73fb04d009afb43570f08
Liu Liu <liuliusquare@gmail.com>
2014-07-27 14:35:59 -0700

    tweak animation and add content

--- script.js
+++ script.js
@@ -3,10 +3,18 @@ $(function(){
 	var sites = [ 
 					'never says never',
 					'thinks before she mocks',
-					'should learn how code works',
+					'should know how code works',
 					'is empathetic',
+					'is obsessed with details',
+					'loves sticky notes',
+					'has a big heart',
 					'asks a lot of questions',
 					'picks the right battle',
+					'is persistent',
+					'should be on Twitter',
+					'does not design with Lorem Ipsum',
+					'knows Photoshop is only a tool',
+					'tells stories with no bullshit',
 					'does more than pushing pixels'
 				];
 
@@ -24,7 +32,7 @@ $(function(){
 		
 		$("#quote").animate({ 
         	opacity: "0",  
-        	marginTop: "0.1em",    
+        	
      	}, 500 );
 
      	
@@ -32,17 +40,16 @@ $(function(){
 		setTimeout(function() {
       		var siteNumber = Math.floor(Math.random()*sites.length);
 			$('#quote').text(sites[siteNumber]);
-		}, 500);
+			$("#quote").css({ 'marginTop': '-0.2em' });  
+		}, 600);
+
 
 		setTimeout(function() {
       		$("#quote").animate({ 
 	        	opacity: "1",
-	        	marginTop: "-0.1em",  
+	        	marginTop: "0em",  
 	     	}, 500 );
-
-	     	
-
-		}, 500);
+		}, 700);
 
 
 		var colorNumber = Math.floor(Math.random()*colors.length);
